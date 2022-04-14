8f4c5c4ee2d1c6cc2d35e12d2bf892ab87d017b0
SaiBingi <saibingi1208@gmail.com>
2022-04-14 19:25:33 +0000

    App Store Solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// Published URL ==>  https://saiplaystore.ccbp.tech
+
+import AppStore from './components/AppStore'
+
+import './App.css'
+
+const App = () => <AppStore />
+
+export default App
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, isActive, changeTabItem} = props
+  const {tabId, displayText} = tabDetails
+
+  const activeTabClassName = isActive ? 'active-tab-item' : ''
+
+  const onClickTabItem = () => {
+    changeTabItem(tabId)
+  }
+
+  return (
+    <li>
+      <button
+        type="button"
+        className={`tab-name ${activeTabClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
